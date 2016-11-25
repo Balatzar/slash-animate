118c80c64c8f829f10e9f4667c63762610b8d4e6
balatzar <bataprod@gmail.com>
2016-11-25 22:31:15 +0100

    test better reponse

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function (req, res) {
   console.log(req.body)
-  res.status(200).send("bonsoir")
+
+  var response = {
+    "response_type": "in_channel",
+    "text": "It's 80 degrees right now.",
+    "attachments": [
+      {
+        "text":"Partly cloudy today and tomorrow"
+      }
+    ]
+  }
+  res.status(200).json(response)
 })
 
 app.listen(process.env.PORT || 5000, function () {
