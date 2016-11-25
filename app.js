efc101dc0fe399ce1d15c7817ac6cf9f4d509755
balatzar <bataprod@gmail.com>
2016-11-25 22:36:35 +0100

    test post

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var post = require("post-json")
 
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function (req, res) {
   console.log(req.body)
+  var response_url = req.body.response_url
 
   var response = {
     "response_type": "in_channel",
     "text": ":full_moon_with_face:",
   }
   res.status(200).json(response)
+
+  post(response_url, response, function (err, result) {
+    if (err) {
+      console.warn(err)
+    } else {
+      console.log(result)
+    }
+  })
 })
 
 app.listen(process.env.PORT || 5000, function () {
