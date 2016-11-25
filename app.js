97b8d80daa76de175c13e36dc68e1059401d3684
balatzar <bataprod@gmail.com>
2016-11-25 22:25:58 +0100

    add body parser

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function (req, res) {
-  console.log(req)
+  console.log(req.body)
   res.status(200)
 })
 
