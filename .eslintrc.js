1c04b257a6194296a58c38229524532783338899
balatzar <bataprod@gmail.com>
2016-11-26 15:23:24 +0100

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "standard",
+    "installedESLint": true,
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "rules": {
+      "quotes": ["error", "double"],
+       "comma-dangle": ["error", "always-multiline"],
+    }
+};
--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express')
+var express = require("express")
 var app = express()
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser")
 var post = require("post-json")
 
-app.use(bodyParser.json({ limit: '5mb' }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "5mb" }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/', function (req, res) {
+app.post("/", function (req, res) {
   console.log(req.body)
-  var url = "https://slack.com/api/chat.postMessage?token=" + process.env.slack + "&channel=%23testage&text=";
+  var url = "https://slack.com/api/chat.postMessage?token=" + process.env.slack + "&channel=%23testage&text="
   var urlText = url + encodeURIComponent(`:house:${"  ".repeat(21)}:runner:`)
 
   console.log(urlText)
@@ -26,19 +26,19 @@ app.post('/', function (req, res) {
       console.log(result.body)
       var msg = JSON.parse(result.body)
 
-      var editUrl = "https://slack.com/api/chat.update?token=" + process.env.slack + "&ts=" + msg.ts +"&channel=" + msg.channel + "&text=";
+      var editUrl = "https://slack.com/api/chat.update?token=" + process.env.slack + "&ts=" + msg.ts + "&channel=" + msg.channel + "&text="
 
       var space = "  "
 
-      var j = 20;
+      var j = 20
 
       var interval = setInterval(() => {
         j -= 1
         if (!j) {
-          clearInterval(interval);
+          clearInterval(interval)
         }
-        var text = `:house:${space.repeat(j)}${j > 1 ? ':runner:' : ''}`
-        
+        var text = `:house:${space.repeat(j)}${j > 1 ? ":runner:" : ""}`
+
         post(editUrl + encodeURIComponent(text), {}, (error, r) => {
           if (error) {
             console.warn(error)
@@ -52,5 +52,5 @@ app.post('/', function (req, res) {
 })
 
 app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log("Example app listening on port 3000!")
 })
