4dcb5a3bd9f8d88c0f80f74a2f47ab941566c7d5
balatzar <bataprod@gmail.com>
2016-11-26 19:08:12 +0100

    update tests

--- src/modules/url_creator.spec.js
+++ src/modules/url_creator.spec.js
@@ -2,9 +2,13 @@ var test = require("tape")
 var urlCreator = require("./url_creator")
 
 test("timing test", (t) => {
-  t.plan(3)
-
   t.equal(urlCreator("https://slack.com/api/", "chat.update", { text: "coucou" }), "https://slack.com/api/chat.update?text=coucou")
   t.equal(urlCreator("https://slack.com/api/", "chat.update"), "https://slack.com/api/chat.update")
   t.equal(urlCreator("https://slack.com/api"), "https://slack.com/api")
+  t.equal(urlCreator("https://slack.com/api/", "chat.update", {
+    text: "coucou",
+    ts: "21202102.321",
+  }), "https://slack.com/api/chat.update?text=coucou&ts=21202102.321")
+
+  t.end()
 })
