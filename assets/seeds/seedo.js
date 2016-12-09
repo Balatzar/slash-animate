ab332792c9a12e14ad5288a963cf85ed34f03a9b
balatzar <bataprod@gmail.com>
2016-12-09 21:29:12 +0100

    remove some logs

--- assets/seeds/seedo.js
+++ assets/seeds/seedo.js
@@ -14,16 +14,12 @@ var lines = never.split("\n")
 for (var i = 1, len = lines.length, j = 0, times = parseInt(lines[0][0] + lines[0][1]); i < len; i += 1) {
   if (i % 14 === 0) {
     json.frames[j] += "```\n"
-    console.log("========")
     console.log(json.frames[j])
-    console.log("========")
     var save = j
     for (var k = 1; k < times; k += 1) {
       json.frames.push(json.frames[save])
       j += 1
-      console.log("========")
       console.log(json.frames[j])
-      console.log("========")
     }
     j += 1
     json.frames[j] = "```\n"
